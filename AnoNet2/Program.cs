4536b19b085471c7bc011ed11d989f144393743f
Alfonso Lara <orozcoenrique159@gmail.com>
2024-06-03 13:38:27 -0500

    primer commit de app

---
+++ AnoNet2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+var cadenaConexion = "Data Source=DESKTOP-FAP3MIT;Database=adoNet;Integrated Security=True;TrustServerCertificate=True";
+
+Console.Write("Digite el id a eliminar: ");
+var nombre = Console.ReadLine(); //1. se declara la variable local.
+
+try
+{
+
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Conexion ha fallado");
+    Console.WriteLine(ex.Message);
+}
+Console.WriteLine("Fin");
